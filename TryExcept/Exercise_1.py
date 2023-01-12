a2199cb9a2a57859f7a22e968071bc5655b1d944
wcc <spurgear@users.noreply.github.com>
2023-01-12 21:48:53 +0000

    Class on Jan 10, 2023.

--- TryExcept/Exercise_1.py
+++ TryExcept/Exercise_1.py
@@ -1,20 +1,26 @@
+import ast
+
 # Create a try/except that will run successfully
 try:
   # Add something that will run successfully
-  
-except:
+  p = open("Exercise_2.py", 'r')
+  ast.parse(p.read())
+
+except SyntaxError as ex:
+  print(f"Got syntax error! {ex}")
+
+except Exception as ex:
+  print(ex)
+  print(type(ex))
   print("try/except 1 produced an error")
 
-
-  
   # Create a try/except that will fail to run
 try:
   # Add something that will not run successfully
-  
-except:
+  1 + 1
+except Exception as ex:
   print("try/except 2 produced an error")
-
-
+  print(ex)
 
 # The whole program should still complete, despite an error occurring above
 print("program did not abort")
--- TryExcept/Exercise_2.py
+++ TryExcept/Exercise_2.py
@@ -4,5 +4,13 @@ def greet(name):
     print(greeting)
   # Add TypeError handling
   # Add a catch-all exception
+  except TypeError:
+    pass
+  except:
+    pass
+
 
 # Test the function below
+
+greet("ABC")
+greet(123)
